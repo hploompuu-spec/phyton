0ffa0ac53c2658d0e8306a138376ad16c290ab87
hploompuu-spec <hploompuu@hkhk.edu.ee>
2025-10-11 12:25:43 +0300

    Valmis

--- h04.py
+++ h04.py
@@ -1,9 +1,27 @@
+# Ringi pindala ja Turtle
+# Kirjuta programm, mis kasutab Turtle graafikat joonistamaks ringi ning arvutab ja kuvab konsoolis ringi pindala ja ümbermõõdu.
+# Programm küsib kasutajalt ringi raadiuse.
+# Kasuta ** operaatorit raadiuse ruudu arvutamiseks ja π väärtusena 3.14.
+# Lisa veakontroll, et kontrollida kasutaja sisestatud raadiuse korrektsust.
+# Väljasta lause, kasutades f-string vormindamist ja ümardamist 2 komakohta
+# Näide:
+# Kasutaja sisestab: 5
+# Programm väljastab konsoolis: Ringi pindala on 78.5 ja ümbermõõt on 31.4
+# Turtle graafika joonistab vastava ringi
+
+import turtle
 
 
+try:
+    r = float(input("Anna mulle ringi raadius r ="))
+    s = 3,14 * r ** 2
+    p = 2 * 3,14 * r
+    print(f"Ringi pindala on {s:0.2f} ja ümbermõõt on {p:.2f}")
+    turtle.circle(r)
+except:
+    print("Kontrolli sisestust!")
 
-
-
-
+turtle.done()
 
 #ALT ja 3 lisab trellid kogu teksti ette
 # 
@@ -58,4 +76,3 @@ b = int(input("Lisa külg 2: "))
 p = (a+b)*2
 
 print(f"Aia kogupikkus on {p}.") #print(f"") lubab lisada muutujad otse teksti sisse, kasutades loogilisi sulge {}
-
