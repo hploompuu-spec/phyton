eaec28e1ab9c68250ab320f27a586eb31b2d55c8
hploompuu-spec <hploompuu@hkhk.edu.ee>
2025-10-25 15:37:46 +0300

    valmis

--- iseseisev01.py
+++ iseseisev01.py
@@ -7,14 +7,18 @@ print("Tere maailm!")
 # 1.2. Aasta liblikas
 # Koostada programm, mille
 # 1. real luuakse muutuja nimega aasta ning antakse sellele väärtuseks 2020 (arvuna);
-aasta = 2020
 # 2. real luuakse muutuja nimega liblikas ning antakse sellele väärtuseks " teelehe-mosaiikliblikas" (sõnena);
-liblikas = "teelehe-mosaiikliblikas"
 # 3. real luuakse muutuja nimega lause_keskosa ning antakse sellele väärtuseks ". aasta liblikas on " (sõnena);
-lause_keskosa = ". aasta liblikas on "
 # 4. real luuakse muutuja nimega lause, mille väärtuse saamiseks ühendatakse üheks sõnaks muutujad aasta, lause_keskosa ja liblikas (vajadusel tuleb kasutada funktsiooni, mis teisendab arvu sõneks);
-lause = str(aasta) + lause_keskosa + liblikas
 # 5. real väljastatakse muutuja lause väärtus ekraanile.
+aasta = 2020
+
+liblikas = "teelehe-mosaiikliblikas"
+
+lause_keskosa = ". aasta liblikas on "
+
+lause = str(aasta) + lause_keskosa + liblikas
+
 print(lause)
 
 # 1.3. Pilved
