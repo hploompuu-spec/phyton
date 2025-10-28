59fe5aef6439e35d051e06dfd5ddd6a188d4b354
hploompuu-spec <hploompuu@hkhk.edu.ee>
2025-10-28 21:44:07 +0200

    Create iseseisev02.py

---
+++ iseseisev02.py
@@ -0,0 +1,16 @@
+# Kodune ülessanne kasutades Turtle ja for käsklusi
+# Kujund 13 fotofailist
+import turtle
+
+for i in range (5):
+    turtle.fd(90)
+    turtle.lt(120)
+    turtle.fd(180)
+    turtle.lt(120)
+    turtle.fd(180)
+    turtle.lt(120)
+    turtle.fd(90)
+    turtle.lt(72)
+
+
+turtle.done()
