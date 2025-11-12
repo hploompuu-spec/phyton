2854c7a0898c76aff6315dc708b180ad2848366f
hploompuu-spec <hploompuu@hkhk.edu.ee>
2025-11-12 16:24:21 +0200

    proov

---
+++ iseseisev03.py
@@ -0,0 +1,17 @@
+import turtle 
+
+# kujund = input("Millist kujundit soovid joonistada?")
+# arv = int(input("Mitu kujundit soovid joonistada?"))
+
+# if kujund == ruut:
+#     for i in range (arv):
+turtle.fd (100)
+turtle.lt (90)
+turtle.fd (100)
+turtle.lt (90)
+turtle.fd (100)
+turtle.lt (90)
+turtle.fd (100)
+turtle.lt (90)
+# else:
+#     print("Proovi uuesti")
